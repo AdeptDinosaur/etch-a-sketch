0bdbd9c37fe82d8cc819e86d4ae2fd15b9bb52c4
AdeptDinosaur <126434367+AdeptDinosaur@users.noreply.github.com>
2023-04-05 19:12:35 -0400

    Add color picker and rainbow

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 squareGrid(64);
 
+let currentColor = 'black';
+
 function resetGrid() {
     const container = document.querySelector('#container');
     container.innerHTML = ''; // Remove the current grid
@@ -14,29 +16,27 @@ function resetGrid() {
     document.getElementById('textInput').value = val;
     resetGrid(); // Call the resetGrid function to remove the current grid and create a new one with the updated value of squares
   }
+// rainbow toggle
 
+const rainbow = document.getElementById('rainbow');
+rainbow.addEventListener('click', () => {
+    if (currentColor !== 'random') {
+        currentColor = 'random';
+    } else {
+        currentColor = 'black';
+    }
+});
 
+//color picker
+const colorPicker = document.getElementById('colorPicker');
+colorPicker.addEventListener('change', (event) => {
+    currentColor = event.target.value;
+})
 
-//input from button to be deprecated...
-window.onload=function(){
-    const btn = document.querySelector('#btn');
-    btn.addEventListener('click', () => {
-        let squares;
-        do { 
-            squares = prompt("Enter squares per side up to 100:");
-            squares = Number(squares);
-            if (squares > 100) {
-                alert("Please enter a number less than 100.")
-            }
-        } while (squares > 100);
-        console.log(squares);
-        squareGrid(squares);
-    })
-}
 
 //establishment of grid of divs for etch board
 
-function squareGrid(squares, color = 'black') {
+function squareGrid(squares) {
     const container = document.getElementById('container');
     let columns = squares;
     let rows = squares;
@@ -48,21 +48,28 @@ function squareGrid(squares, color = 'black') {
             row.className = 'row';
             column.appendChild(row);
             
-            let rainbow = document.querySelector('#rainbow')
+          
             
             row.addEventListener('mouseover', () => {
-                rainbow.addEventListener('click', function(ev){
-                    if (ev.currentTarget.id == true){
-                        row.style.backgroundColor = 'red';
-                    }
-                })
-                row.style.backgroundColor = color;
+                if (currentColor === 'black') {
+                    row.style.backgroundColor = 'black';
+                } else if (currentColor === 'random') {
+                    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                    row.style.backgroundColor = randomColor;
+                } else {
+                    row.style.backgroundColor = currentColor;
+                }   
             })
         }
         container.appendChild(column);
 
     }
 }
+
+
+
+
+
 const clear = document.getElementById('clear');
 clear.addEventListener('click', () => {
     resetGrid();
